faa2dcd7cffde6a09f546c6deee5ea60169562f4
rahulverma307 <156118545+rahulverma307@users.noreply.github.com>
2025-04-26 03:40:53 +0000

    02_datatypes.js

---
+++  01_Basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; //this treat all js code as newer version.
+
+//alert("hello") we are using nodejs,not browser this show error on that
+
+console.log(3+
+    3)// code readability should be high 
+
+console.log("rahul")// code readability should be high 
+
+let name="Rahul"
+let age=18;
+let isLoggedIn=false;
+
+/*
+primitive data type====
+
+number=>2 to power53
+bignit
+string=>""
+boolean=> true/false
+null=>
+undefined=> valu not defined
+symbol=> used for find unique
+ =============
+
+ object
+ */
+console.log(typeof null);// this type is object
+console.log(typeof undefined); //undefined
+
+
