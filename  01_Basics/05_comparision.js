dd4698323af68f568a3f966d8855128611eb7b3d
rahulverma307 <156118545+rahulverma307@users.noreply.github.com>
2025-04-26 03:42:37 +0000

    05_comparision

---
+++  01_Basics/05_comparision.js
@@ -0,0 +1,33 @@
+console.log(2>1);
+console.log(2==1);
+console.log(2>=1);
+console.log(2!=1);
+console.log(2<1);
+
+console.log("2">0); //convert string to number// true
+console.log("02">1);//true
+
+
+
+console.log(null>0);// null is converted into numeric zero // out=>false
+console.log(null==0);// in that null is not eqaul to zero// out=>false
+console.log(null>=0); // null conert to zero and // output is true
+
+console.log(undefined=0); // this is not comaprision this assignment
+console.log(undefined==0); // undefined is not equal to zero
+console.log(undefined>0);// undefined conevrt to Nan retunt false
+console.log(undefined<0); //NaN */
+ //// most of that we avoid above comparision in industry
+
+////****************Strict Check 
+
+console.log("2"===2);
+
+
+
+
+
+
+
+
+
