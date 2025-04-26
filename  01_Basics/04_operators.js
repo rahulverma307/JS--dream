48ca45161409ad35bcb58c9e8d26ec242954bd16
rahulverma307 <156118545+rahulverma307@users.noreply.github.com>
2025-04-26 03:42:15 +0000

    04_operators

---
+++  01_Basics/04_operators.js
@@ -0,0 +1,25 @@
+let value=3
+// let negvalue=-value
+console.log(value);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2%2);
+//console.log(2/2);
+let str1="hello"
+let str2="hittes"
+let str3=str1+str2;
+
+console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2); //agar first string hai to next value ko string concider kiya jaiga 
+// console.log(1+2+"2");// agar ye hai to baad me string concider kiya jayaga
+
+let gameCounter=100;
+++gameCounter;//
+console.log(gameCounter);
+
+
+
