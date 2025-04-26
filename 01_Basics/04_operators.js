f3b29afff7a87f37362040eb8601e5cc02f47a80
rahulverma307 <156118545+rahulverma307@users.noreply.github.com>
2025-04-26 09:13:54 +0530

    Update 04_operators.js

--- 01_Basics/04_operators.js
+++ 01_Basics/04_operators.js
@@ -14,7 +14,7 @@ console.log(str3)
 
 // console.log("1"+2);
 // console.log(1+"2");
-// console.log("1"+2+2); //agar first string hai to next value ko string concider kiya jaiga 
+// console.log("1"+2+2); //agar first string hai to next value ko string consider kiya jaiga 
 // console.log(1+2+"2");// agar ye hai to baad me string concider kiya jayaga
 
 let gameCounter=100;
