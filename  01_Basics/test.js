fca9504194c7375354ab5ad2cf37f72c35c9b3d9
rahulverma307 <156118545+rahulverma307@users.noreply.github.com>
2025-04-29 17:22:20 +0000

    test

---  01_Basics/test.js
+++  01_Basics/test.js
@@ -1,1 +1,16 @@
-console.log("jai shree ram");
+// console.log("jai shree ram");
+
+let balance=new Number("123.456");
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed());
+// // 
+// console.log(balance.toPrecision(3));
+
+
+console.log(parseInt(typeof "123abs"));
+
+
+
+
+
