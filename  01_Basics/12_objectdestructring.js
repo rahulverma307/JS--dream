1afa3ceb07980d94ea6864334869183bdcf72370
rahulverma307 <156118545+rahulverma307@users.noreply.github.com>
2025-04-29 17:46:27 +0000

    12_apijson

---
+++  01_Basics/12_objectdestructring.js
@@ -0,0 +1,30 @@
+//Destructuring==
+
+const course={
+    coursename:"js",
+    price:99,
+    courseIntructor:"hitesh"
+}
+
+//course.couseIntructor
+//destructure of object
+const {courseIntructor:intructor}=course
+// console.log(courseIntructor);//we use one of them for print
+// console.log(intructor);
+
+///JSON
+// {
+//     //this is JSON API
+//     "name":"rahul",
+//     "Course":"js hindi"
+//     "PRICE":"free"
+// }
+
+// [
+//     {},
+//     {},  json ApI
+//     {}
+// ]
+
+
+
