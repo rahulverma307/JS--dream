cc58a19cb0318739de9205d7b07587c9df266d00
rahulverma307 <156118545+rahulverma307@users.noreply.github.com>
2025-05-02 16:43:49 +0000

    ..

---  01_Basics/14_arrowfunction.js
+++  01_Basics/14_arrowfunction.js
@@ -1,12 +1,43 @@
-const user={
-    username:"rahul",
-    price:987,
+//this means "this object" — the object where the function is written.
 
-    welcomeMessage:function(){
-        console.log(`${this.username},welcome sir ji`);//this keyword current content me video print karta hai
+
+// const user={
+//     username:"rahul",
+//     price:987,
+// this keyword works in JavaScript when used inside an object method.
+//     welcomeMessage:function(){
+//         console.log(`${this.username},welcome sir ji`);//this keyword current content me video print karta hai
         
-    }
+//     }
+// }
+// user.welcomeMessage()
+// user.username="vivek"//this.username always looks at the current value of username in the same object.
+// user.welcomeMessage()
+
+// function chai(){
+//     let username="rahul"
+//     console.log(this.username) //this not use in function.only use in object
+    
+// }
+// chai()
+
+
+//arrow function
+
+const chai = () => {
+  let username="rahuke"
+  console.log(this.username);//this keyword not work in arrow function 
+  
 }
-user.welcomeMessage()
-user.username="vivek"
-user.welcomeMessage()
+chai()
+
+// const addtwo = (num1,num2) =>{
+//    return   num1+num2
+// }
+
+//implicit return we dont use curly parenthesis
+// const addtwo = (num1,num2) => (num1+num2)
+
+const addtwo = (num1,num2) =>({username:"rahul"}) //for object in arrow function
+
+console.log(addtwo(45,45));
---
+++  01_Basics/15_iife.js
@@ -0,0 +1,14 @@
+//immediately invoked function expressions (IIFE)
+
+(function chai(){
+    console.log("db connected");
+    
+})(); //db conneted
+// chai()
+
+
+   ( function aurcode(){
+    console.log("jia shree ram");
+    
+}) ()  
+    
