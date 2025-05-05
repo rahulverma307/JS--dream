80532912c69c254f9763deb56e54449b2e416e4c
rahulverma307 <156118545+rahulverma307@users.noreply.github.com>
2025-05-05 06:54:17 +0000

    .

---  01_Basics/test.js
+++  01_Basics/test.js
@@ -1,14 +1,54 @@
-// console.log("jai shree ram");
+// // // console.log("jai shree ram");
 
-let balance=new Number("123.456");
-// console.log(balance.toString().length);
+// // let balance=new Number("123.456");
+// // // console.log(balance.toString().length);
 
-// console.log(balance.toFixed());
-// // 
-// console.log(balance.toPrecision(3));
+// // // console.log(balance.toFixed());
+// // // // 
+// // // console.log(balance.toPrecision(3));
 
 
-console.log(parseInt(typeof "123abs"));
+// // console.log(parseInt(typeof "123abs"));
+
+
+// // const foo= null;
+// // console.log(typeof foo);
+
+// // let name ="ankur"
+// // let anothername=name
+// // anothername="sakshi"
+// // console.log(name);
+// // console.log(anothername);
+
+// let school={
+//     name:"navankur",
+//     class:"12th",
+//     rollno:4545
+// }
+// let college=school
+// college.name="ram"
+// console.log(school);
+
+// // college.name="neha"
+// // console.log(school.name);
+// // console.log(college);
+// // console.log(school);
+
+
+
+
+///number and MAth
+
+// console.log(Number.isInteger(value))
+// const value=25
+// console.log(Number.isInteger(25))
+// console.log(value.toString());
+// console.log(typeof value);
+
+const time =Date.now()
+let timee=time/1000
+console.log(Math.round(Math.floor(("time in second",time/1000))))
+
 
 
 
