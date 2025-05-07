08c778c6b4164007354ebc74fb8938005e5d1664
rahulverma307 <156118545+rahulverma307@users.noreply.github.com>
2025-05-07 06:36:04 +0000

    forof

---
+++  01_Basics/19_high_order_array_loop.js
@@ -0,0 +1,41 @@
+ // forof
+
+// ["","",""]
+//[{},{},{}]//use on array,object ,strung
+
+// const arr=[1,2,3,4,5,98,45]
+// for (const num of arr) {
+//     console.log(num);
+    
+// }
+////
+// const greetings="hello world!"
+// for (const greet of greetings) {
+//     console.log(greet);
+    
+// }
+
+///"Maps" is a object that hold a key-value pair.it is a unique vaku pair
+
+// const map=new Map()
+// map.set("delhi","agra")
+// map.set("agra","mumbai")
+// map.set("punjab","amritsra")
+// console.log(map);
+
+// for (const [key,value] of map) {
+//     console.log(key,"=>",value);
+    
+// }
+
+
+///
+
+const myObject={
+    game1:'nfs',
+    game2:"nfs2"
+}
+// for (const [key,value] of myObject) {
+//     console.log(key,":",value);
+    
+// }
