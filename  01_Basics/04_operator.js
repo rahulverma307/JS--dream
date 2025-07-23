60fdde5636338851b21aba1c33c7c47719c13aa6
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2025-07-23 07:03:23 +0000

    operator

---  01_Basics/04_operator.js
+++  01_Basics/04_operator.js
@@ -1,24 +1,33 @@
-let value=3
-// let negvalue=-value
+let value = 3
+    // let negvalue=-value
 console.log(value);
-//console.log(2+2);
-//console.log(2-2);
-//console.log(2*2);
-//console.log(2%2);
-//console.log(2/2);
-let str1="hello"
-let str2="hittes"
-let str3=str1+str2;
+console.log(2 + 2);
+console.log(2 - 2);
+console.log(2 * 2);
+console.log(2 % 2);
+console.log(2 / 2);
+let str1 = "hello"
+let str2 = "hittes"
+let str3 = str1 + str2;
 
 console.log(str3)
 
-// console.log("1"+2);
-// console.log(1+"2");
-// console.log("1"+2+2); //agar first string hai to next value ko string consider kiya jaiga 
-// console.log(1+2+"2");// agar ye hai to baad me string concider kiya jayaga
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2); //agar first string hai to next value ko string consider kiya jaiga 
+console.log(1 + 2 + "2"); // agar ye hai to baad me string concider kiya jayaga
 
-let gameCounter=100;
-++gameCounter;//
+let score = 1 + 2 + "22"
+console.log("add number and string", score);
+console.log(typeof score); // type is string
+
+let num = 1
+console.log(++num)
+console.log(num);
+
+
+
+let gameCounter = 100;
+++gameCounter; //
 console.log(gameCounter);
-
-
+console.log(gameCounter);
