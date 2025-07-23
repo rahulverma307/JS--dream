1a13c474d80128daa98ec5294d3007c7a9ee4931
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2025-07-23 06:41:23 +0000

    datatype

---  01_Basics/03_datatype_conversion.js
+++  01_Basics/03_datatype_conversion.js
@@ -1,24 +1,24 @@
-let score="33"
+let score = "33"
 console.log(typeof score);
 
-let valueInnumber=Number(score)
-console.log(typeof valueInnumber  );
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber);
 // we write "33"=> easily convert into  number 33
 //"33abc"=> give output NaN but type is number
 
 
-let isLoggedIn="hitesh"
+let isLoggedIn = "hitesh"
 
-let booleanIsLoggedIn=Boolean(isLoggedIn)
+let booleanIsLoggedIn = Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
 
 // 1=> true; 0=>false
-//""we use empty than ouput become=> false
+//""we use empty string than ouput become=> false
 //"hitesh"=> true
 
 
-let someNumber=33
-let StringNumber= String(someNumber);
+let someNumber = 33
+let StringNumber = String(someNumber);
 console.log(StringNumber);
 console.log(typeof StringNumber);
 // 33 number=> convert into string
