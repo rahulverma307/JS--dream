cd0edf9bdbad97a12df375953cc70b84c37a5435
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2025-07-23 06:38:22 +0000

    practice

---
+++  01_Basics/00_practice.js
@@ -0,0 +1,16 @@
+let age = 33
+console.log(typeof age);
+
+
+
+let score = "rahul33"
+console.log(typeof(score));
+let NewScore = Number(score)
+console.log(NewScore);
+
+console.log(typeof NewScore);
+
+let name = 1
+let namee = Boolean(name)
+console.log(namee);
+console.log(typeof name);
