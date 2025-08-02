f485b74da385ae99339715eb85c26a3b0f8f8743
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2025-08-02 08:55:15 +0000

    array

---
+++ 02_Basics/arrays.js
@@ -0,0 +1,18 @@
+//                                                                     console.log("Jai Shree Ram");
+// const myArr = [0, 1, 2, 3, 4, 5, true, "hitesh"]
+
+// Array method
+// myArr.push(6) //add value in last
+// myArr.push(7)
+// myArr.pop() // remove last value 
+
+// myArr.unshift(0) // add value in start
+// myArr.shift() // remove value from the start value
+
+// console.log(myArr.includes(9)); // check value is present or not =>T/F
+// console.log(myArr.indexOf(8));
+// const newArr = myArr.join() // convert into string
+// console.log(newArr);
+
+// console.log(myArr); //[ 0, 1, 2, 3, 4, 5, true, 'hitesh', 6 ]
+const name =
