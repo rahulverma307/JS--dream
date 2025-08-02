768d4c1cdcbf1a7e2ac2286f0beec4444a9dccb7
Rahul Verma <156118545+rahulverma307@users.noreply.github.com>
2025-08-02 09:26:46 +0000

    ..

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,17 @@
+const marvel_heros = ["ionmane", "spiderman", "thor", "hulk"];
+const Dc_heroes = ["superman", "batman", "green lantern"];
+
+// marvel_heros.push(Dc_heroes)
+// console.log(marvel_heros); // this Create array inside array
+
+
+// const allhero = marvel_heros.concat(Dc_heroes)
+// console.log(allhero); // this add both array without any array inside array
+
+
+// const allnewhero = [...marvel_heros, ...Dc_heroes] // spread operator
+// console.log(allnewhero);
+
+const New_Array = [1, 2, 3, 4, [45, 65, 78, ],
+    [545454],
+]
--- 02_Basics/arrays.js
+++ 02_Basics/arrays.js
@@ -15,4 +15,13 @@
 // console.log(newArr);
 
 // console.log(myArr); //[ 0, 1, 2, 3, 4, 5, true, 'hitesh', 6 ]
-const name =
+
+// SLICE/ SPLICE====
+// const name = [0, 1, 2, 3, 4, 5, 6]
+// console.log("A", name);
+// console.log(name.slice(1, 3)); // slice does not change the original array.
+// console.log(name);//[0, 1, 2, 3, 4, 5, 6]
+
+// const age = [45, 85, 98, 45, 78, 12]
+// console.log(age.splice(1, 2)); //[ 85, 98 ]
+// console.log(age); // it is change the original array//[45, 45, 78, 12]
